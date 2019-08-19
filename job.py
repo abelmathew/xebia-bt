2c45ff3b15c6c96f1694b9ffaddbd2000f8b86bd
Abel Mathew <amathew@backtrace.io>
2019-08-19 15:01:04 +0000

    small fixes

--- job.py
+++ job.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python
 import sys, os
 #insert file into path for code capture
 root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, root_dir)
 import backtracepython as bt
-
-host, port = sys.argv[1:]
 
 bt.initialize(
     endpoint="https://submit.backtrace.io/testing-xebialabs/e958cfd2940e7bdb2d60fdf7fd22d4caaecdb9966efc835e24054a247aef6162/json",
